175202d891b2cbed89fae2bdfcf5b5d5356fbdb6
terminalforlife <terminalforlife@yahoo.com>
2020-03-12 02:20:28 +0000

    Add more to Love game

--- source/love/main.lua
+++ source/love/main.lua
@@ -11,8 +11,36 @@
 -- 2D game development framework for the Lua programming language.
 -------------------------------------------------------------------------------
 
+XNow = 0
+YNow = 0
+
 function love.draw()
 	love.graphics.print('Demonstration.', 100, 100)
+
+	-- Draw a one-pixel line around the screen. Target_X, Target_Y, -
+	-- Destination_X, then Destination_Y.
+	love.graphics.line(0, 0, 0, 1080)
+	love.graphics.line(0, 0, 1920, 0)
+	love.graphics.line(1920, 0, 1920, 1080)
+	love.graphics.line(1920, 1080, 0, 1080)
+
+	love.graphics.setColor(255, 0, 0, 200)
+
+	love.graphics.line(XNow + 1, 100, 200, 200)
+end
+
+function love.update(delta)
+	if XNow < love.graphics.getWidth() then
+		XNow = XNow + 100 * delta
+	else
+		XNow = 0
+	end
+end
+
+function love.load()
+	print('Loading happens now.')
+
+	love.graphics.setBackgroundColor(20, 20, 20, 255)
 end
 
 function love.quit()
